250d87a578861e4355d030df8e5ebf833f1807cf
James Stidard <james@stidard.com>
2019-07-17 00:44:17 +0100

    quick open

--- configs/fish/functions/pyground.fish
+++ configs/fish/functions/pyground.fish
@@ -3,8 +3,10 @@ function pyground
 
     cd $dir
 
-    pipenv install --pre black flake8 flake8-bugbear $argv --skip-lock
+    pipenv install --skip-lock
 
     touch main.py
     code $dir main.py
+
+    pipenv install $argv --pre black flake8 flake8-bugbear
 end
