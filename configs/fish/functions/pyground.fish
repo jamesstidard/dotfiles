8ed6dd66b2d27812f3b18c0f85da915536e5dbf0
James Stidard <james@stidard.com>
2019-07-17 19:15:20 +0100

    vscode settings

--- configs/fish/functions/pyground.fish
+++ configs/fish/functions/pyground.fish
@@ -5,7 +5,35 @@ function pyground
 
     pipenv --bare install --skip-lock
 
-    touch main.py
+    mkdir .vscode
+
+    set --local launch \
+'{
+    // Use IntelliSense to learn about possible attributes.
+    // Hover to view descriptions of existing attributes.
+    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "Current File",
+            "type": "python",
+            "request": "launch",
+            "program": "${file}",
+            "console": "integratedTerminal",
+            "envFile": "${workspaceFolder}/.env"
+        }
+    ]
+}'
+    echo $launch > .vscode/launch.json
+
+    set --local settings \
+'{
+    "python.pythonPath": "'(pipenv --py)'"
+}'
+    echo $settings > .vscode/settings.json
+
+    touch {main.py,.env}
+
     code $dir main.py
 
     pipenv install $argv --pre black flake8 flake8-bugbear
