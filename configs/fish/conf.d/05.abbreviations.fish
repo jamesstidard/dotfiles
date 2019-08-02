824255a8c220d90b238d85e6158c994e9c3bbaff
James Stidard <james@wave-venture.com>
2019-08-02 10:24:30 +0100

    fish abbriviations

--- configs/fish/conf.d/05.abbreviations.fish
+++ configs/fish/conf.d/05.abbreviations.fish
@@ -1,12 +1,21 @@
 abbr -a up update
 
 abbr -a gc git clone
+abbr -a gs git status
 abbr -a gx git pull
 abbr -a gp git push
 abbr -a ga git add
 
-abbr -a merge smerge
+abbr -a sm smerge
 
 abbr -a dc docker-compose
 
 abbr -a pg pyground
+
+abbr -a pi pipenv install
+abbr -a pu pipenv uninstall
+
+abbr -a bi brew install
+abbr -a bu brew uninstall
+abbr -a bci brew cask install
+abbr -a bcu brew cask uninstall
