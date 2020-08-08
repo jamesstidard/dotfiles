eb3302693278177a0945d3ce8709a88ec400271c
James Stidard <james@stidard.com>
2020-08-08 13:49:57 +0100

    misc

--- configs/fish/conf.d/profile.fish
+++ configs/fish/conf.d/profile.fish
@@ -1,1 +1,4 @@
 # mostly all set globally via ~fish/functions/set_fish_profile for quick loads
+
+# iTerm shell integration
+test -e ~/.iterm2_shell_integration.fish ; and source ~/.iterm2_shell_integration.fish ; or true
