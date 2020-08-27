19a413231569f71575116729761fe57910fd51a9
James Stidard <james@stidard.com>
2020-08-27 16:38:31 +0100

    no dot

--- configs/fish/functions/set_fish_profile.fish
+++ configs/fish/functions/set_fish_profile.fish
@@ -105,8 +105,8 @@ function set_fish_profile
     abbr -a gsms git submodule sync
     abbr -a gsmu git submodule update --recursive
 
-    abbr -a iterm open -a iTerm .
-    abbr -a term open -a iTerm .
+    abbr -a iterm open -a iTerm
+    abbr -a term open -a iTerm
     abbr -a mta marta
 
     abbr -a c code
