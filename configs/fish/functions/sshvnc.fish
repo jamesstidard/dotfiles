b9afaf470b420a26be625241b8bbee50231c8650
James Stidard <james@stidard.com>
2021-06-03 23:32:19 +0100

    comment

--- configs/fish/functions/sshvnc.fish
+++ configs/fish/functions/sshvnc.fish
@@ -1,4 +1,5 @@
 function sshvnc --description 'Opens host in vnc viewer via ssh tunnel'
+    # Ref: https://gist.github.com/scy/6781836
     ssh -f -o ExitOnForwardFailure=yes -L 5901:localhost:5900 $argv sleep 10
     open vnc://localhost:5901
 end
